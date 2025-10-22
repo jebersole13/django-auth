866e1e852a04735e6250471b40ab905d7a71c1f2
Jon <105177588+jebersole13@users.noreply.github.com>
2025-10-22 15:55:55 +0900

    utcnow() deprecated fix

--- core/authentication.py
+++ core/authentication.py
@@ -22,8 +22,8 @@ class JWTAuthentication(BaseAuthentication):
 def create_access_token(id):
     return jwt.encode({
         'user_id': id,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30),
-        'iat': datetime.datetime.utcnow()
+        'exp': datetime.datetime.now(datetime.UTC) + datetime.timedelta(seconds=30),
+        'iat': datetime.datetime.now(datetime.UTC)
     }, 'access_secret', algorithm='HS256')
 
 
